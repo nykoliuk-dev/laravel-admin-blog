3c7072d5261d193a34578fa0801b94df8ab8577d
Roman <webdeveloverroman@gmail.com>
2025-12-23 14:59:13 +0200

    Migrate layout from Twig to Blade - Replace static URLs with asset() - Add Locale for lang - Add a default value for title - if/else for section header - Add stack for scripts - Edit routes to menu links - Add active menu class

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="UTF-8" />
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>{{ $title }}</title>
+    <title>@yield('title', config('app.name'))</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400" rel="stylesheet" />
-    <link href="/public/assets/css/templatemo-style.css" rel="stylesheet" />
+    <link href="{{ asset('assets/css/templatemo-style.css') }}" rel="stylesheet" />
 </head>
 <body>
 
@@ -15,31 +15,44 @@
     <!-- Top box -->
     <!-- Logo & Site Name -->
     <div class="placeholder">
-        <div class="parallax-window" data-parallax="scroll" data-image-src="/public/assets/img/simple-house-01.jpg">
+        <div class="parallax-window" data-parallax="scroll" data-image-src="{{ asset('assets/img/simple-house-01.jpg') }}">
             <div class="tm-header">
-                <div class="row tm-header-inner">
-                    <div class="col-md-6 col-12">
-                        <img src="/public/assets/img/simple-house-logo.png" alt="Logo" class="tm-site-logo" />
-                        <div class="tm-site-text-box">
-                            <h1 class="tm-site-title">Simple House</h1>
-                            <h6 class="tm-site-description">new restaurant template</h6>
+                @hasSection('header')
+                    @yield('header')
+                @else
+                    <div class="row tm-header-inner">
+                        <div class="col-md-6 col-12">
+                            <img src="{{ asset('assets/img/simple-house-logo.png') }}" alt="Logo" class="tm-site-logo" />
+                            <div class="tm-site-text-box">
+                                <h1 class="tm-site-title">Simple House</h1>
+                                <h6 class="tm-site-description">new restaurant template</h6>
+                            </div>
                         </div>
+                        <nav class="col-md-6 col-12 tm-nav">
+                            <ul class="tm-nav-ul">
+                                <li class="tm-nav-li">
+                                    <a href="{{ route('main.index') }}" class="tm-nav-link {{ Route::is('main.index') ? 'active' : '' }}">Home</a>
+                                </li>
+                                <li class="tm-nav-li">
+                                    <a href="{{ route('posts.index') }}" class="tm-nav-link {{ Route::is('posts.*') ? 'active' : '' }}">Blog</a>
+                                </li>
+                                <li class="tm-nav-li">
+                                    <a href="{{ route('pages.about') }}" class="tm-nav-link {{ Route::is('pages.about') ? 'active' : '' }}">About</a>
+                                </li>
+                                <li class="tm-nav-li">
+                                    <a href="{{ route('pages.contact') }}" class="tm-nav-link {{ Route::is('pages.contact') ? 'active' : '' }}">Contact</a>
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
-                    <nav class="col-md-6 col-12 tm-nav">
-                        <ul class="tm-nav-ul">
-                            <li class="tm-nav-li"><a href="/" class="tm-nav-link active">Home</a></li>
-                            <li class="tm-nav-li"><a href="about.html" class="tm-nav-link">About</a></li>
-                            <li class="tm-nav-li"><a href="contact.html" class="tm-nav-link">Contact</a></li>
-                        </ul>
-                    </nav>
-                </div>
+                @endif
             </div>
         </div>
     </div>
 
     <main>
 
-        {% block content %}{% endblock %}
+        @yield('content')
 
     </main>
 
@@ -49,14 +62,14 @@
             | Design: <a rel="nofollow" href="https://templatemo.com">TemplateMo</a></p>
     </footer>
 </div>
-<script src="/public/assets/js/jquery.min.js"></script>
-<script src="/public/assets/js/parallax.min.js"></script>
-{% block scripts %}{% endblock %}
+<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/js/parallax.min.js') }}"></script>
+@stack('scripts')
 <script>
     $(document).ready(function(){
         // Handle click on paging links
         $('.tm-paging-link').click(function(e){
-            //e.preventDefault();
+            e.preventDefault();
 
             var page = $(this).text().toLowerCase();
             $('.tm-gallery-page').addClass('hidden');
