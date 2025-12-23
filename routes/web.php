f648f51d2a35eeaf71c0f6677c057042492fa3b8
Roman <webdeveloverroman@gmail.com>
2025-12-23 21:21:57 +0200

    Add name to home route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::view('/about', 'pages.about')->name('pages.about');
 Route::view('/contact', 'pages.contact')->name('pages.contact');
