cf0b5aeb162d7b466d92b13f8c687e7845b13e16
Roman <webdeveloverroman@gmail.com>
2025-12-26 16:14:08 +0200

    Add store, show actions to PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Comment;
 use App\Models\Post;
 use App\Models\Tag;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Illuminate\View\View;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
         $posts = Post::latest()->get();
 
@@ -25,7 +32,7 @@ class PostController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
         $categories = Category::all();
         $tags = Tag::all();
@@ -40,17 +47,72 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-        //
+        $rules = [
+            'title'   => 'required|min:3',
+            'content' => 'required',
+            'file'    => 'required|image|max:1024',
+
+            // === Categories ===
+            'categories'      => 'array',
+            'categories.*'    => 'integer|min:1',
+
+            // === Tags ===
+            'tags'            => 'array',
+            'tags.*'          => 'integer|min:1',
+        ];
+
+        $validated = $request->validate($rules);
+
+        $file = $request->file('file');
+        $filename = Str::uuid() . '.' . $file->getClientOriginalExtension();
+        $file->move(
+            public_path('assets/img/gallery'),
+            $filename
+        );
+        $validated['user_id'] = auth()->id();
+
+        try {
+            DB::transaction(function () use ($validated, $request, $filename) {
+                $post = Post::create([
+                    'title'      => $validated['title'],
+                    'slug'       => Str::slug($validated['title']),
+                    'content'    => $validated['content'],
+                    'image_name' => $filename,
+                    'user_id'    => $validated['user_id'],
+                ]);
+
+                $post->categories()->sync($validated['categories'] ?? []);
+                $post->tags()->sync($validated['tags'] ?? []);
+            });
+
+            return response()->json([
+                'success' => true,
+                'message' => "Пост {$validated['user_id']} успешно добавлен!",
+            ]);
+        } catch (\Throwable $e) {
+            Log::error($e);
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Ошибка при создании поста.',
+            ], 500);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Post $post): View
     {
-        //
+        return view('posts.show', [
+            'title' => $post->title,
+            'post' => $post,
+            'categories' => $post->categories,
+            'tags' => $post->tags,
+            'comments' => $post->comments,
+        ]);
     }
 
     /**
