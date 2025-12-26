497434fab75c4f1229aeeed50a3b2c8e15fdf4bc
Roman <webdeveloverroman@gmail.com>
2025-12-26 16:15:01 +0200

    Add index action to MainController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('posts.index', [
+            'title' => 'Список постов',
+            'posts' => $posts,
+        ]);
+    }
 }
