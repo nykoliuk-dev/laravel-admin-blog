c5f3b606c7337def20cfeeceb7c69941d7474d3b
Roman <webdeveloverroman@gmail.com>
2026-01-23 12:51:09 +0200

    Add cast for slug

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\RoleSlug;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
@@ -9,5 +10,9 @@ class Role extends Model
     protected $fillable = [
         'name',
         'slug',
+    ];
+
+    protected $casts = [
+        'slug' => RoleSlug::class,
     ];
 }
