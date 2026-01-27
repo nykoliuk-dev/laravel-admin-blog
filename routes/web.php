b55b19f43cd02eb91e0414a3db4fb181be855902
Roman <webdeveloverroman@gmail.com>
2026-01-27 09:19:02 +0200

    Remove Breeze and switch to Fortify authentication

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StoreCommentController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,18 +21,6 @@ Route::resource('posts', PostController::class)
 Route::post('/posts/{post}/comments', StoreCommentController::class)
     ->middleware('EnsureJsonRequest')
     ->name('posts.comments.store');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
 
 Route::fallback(function () {
     abort(404, 'Oops! Page not found...');
