a0e75922b5de5268ff668ae44e5c1388c3bc2bda
Roman <webdeveloverroman@gmail.com>
2026-02-04 13:14:46 +0200

    feat(tags): implement basic index page

---
+++ resources/views/admin/tags/index.blade.php
@@ -0,0 +1,93 @@
+@extends ('layouts.admin')
+
+@section('title', 'AdminLTE 3 | Tag List Page')
+
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Tag Management</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Blank Page</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header d-flex">
+                                <h3 class="card-title">Tags Table</h3>
+                                <a href="" class="btn btn-primary ml-auto">Add tag</a>
+                            </div>
+                            <!-- /.card-header -->
+                            @if($tags)
+                                <div class="card-body">
+                                    <table class="table table-bordered">
+                                        <thead>
+                                        <tr>
+                                            <th style="width: 10px">#</th>
+                                            <th>Name</th>
+                                            <th>Slug</th>
+                                            <th>Usage</th>
+                                            <th style="width: 40px">Actions</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($tags as $tag)
+                                            <tr>
+                                                <td>{{ $tag->id }}.</td>
+                                                <td>{{ $tag->name }}</td>
+                                                <td>{{ $tag->slug }}</td>
+                                                <td></td>
+                                                <td class="d-flex gap-2">
+                                                    <a href="" class="btn btn-info btn-sm">
+                                                        <i class="fas fa-pencil-alt"></i>
+                                                    </a>
+                                                    <form action="" method="POST">
+                                                        @csrf
+                                                        @method('delete')
+                                                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')">
+                                                            <i class="fas fa-trash-alt"></i>
+                                                        </button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <!-- /.card-body -->
+                                <div class="card-footer clearfix">
+                                    <ul class="pagination pagination-sm m-0 float-right">
+                                        <li class="page-item"><a class="page-link" href="#">«</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">»</a></li>
+                                    </ul>
+                            </div>
+                            @else
+                                <p>No tags yet...</p>
+                            @endif
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        <!-- /.content -->
+    </div>
+@endsection
