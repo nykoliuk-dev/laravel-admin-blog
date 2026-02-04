acf6c83b1013a850b5bebbe08fe56788fbd0ebfb
Roman <webdeveloverroman@gmail.com>
2026-02-04 13:45:08 +0200

    feat(admin): register dashboard, post and tag routes

--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,27 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')
-    ->middleware('auth')
+    ->middleware(['auth', 'admin'])
     ->group(function () {
+        // Main Page
         Route::get('/', DashboardController::class)->name('admin');
+
+
+        // Posts
+        Route::resource('posts', PostController::class)
+            ->scoped([
+                'post' => 'slug',
+            ]);
+
+
+        // Tags
+        Route::resource('tags', TagController::class)
+            ->scoped([
+                'tag' => 'slug',
+            ]);
     });
