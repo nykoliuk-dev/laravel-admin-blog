7016b140b07bf1642417a305e5cad147172db6bd
Roman <webdeveloverroman@gmail.com>
2026-02-12 10:58:54 +0200

    feat(tags-posts): add TagPostsPaginatedQuery

---
+++ app/Domain/Tags/Queries/TagPostsPaginatedQuery.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domain\Tags\Queries;
+
+use App\Domain\Posts\DTO\PostViewDTO;
+use App\Models\Post;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+final class TagPostsPaginatedQuery
+{
+    public function execute(int $page, int $perPage, int $tagId): LengthAwarePaginator
+    {
+        return Post::query()
+            ->whereRelation('tags', 'post_tag.tag_id', $tagId)
+            ->orderBy('id', 'desc')
+            ->paginate(
+                perPage: $perPage,
+                page: $page,
+            )->through(
+                fn (Post $post) => new PostViewDTO(
+                    id: $post->id,
+                    title: $post->title,
+                    slug: $post->slug,
+                    content: $post->content,
+                    imageUrl: $post->image_url,
+                ));
+    }
+}
