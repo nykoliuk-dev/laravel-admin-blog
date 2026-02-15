3d285c4c90d4b428602bd5cecb9b8dd9179be1f4
Roman <webdeveloverroman@gmail.com>
2026-02-15 10:51:41 +0200

    feat(posts): add store method to PostController

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Domain\Categories\Queries\CategoryTreeQuery;
+use App\Domain\Posts\Commands\CreatePostCommand;
+use App\Domain\Posts\Handlers\CreatePostHandler;
+use App\Domain\Posts\Queries\PostDetailsQuery;
 use App\Domain\Posts\Queries\PostListQuery;
 use App\Domain\Tags\Queries\TagForSelectQuery;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -46,9 +53,21 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store()
+    public function store(StorePostRequest $request, CreatePostHandler $postHandler): RedirectResponse
     {
+        $postData = new CreatePostCommand(
+            title: $request->validated('title'),
+            content: $request->validated('content'),
+            userId: auth()->id(),
+            file: $request->file('file'),
+            categories: $request->validated('categories', []),
+            tags: $request->validated('tags', [])
+        );
+        $post = $postHandler->handle($postData);
 
+        return redirect()
+            ->route('admin.posts.index')
+            ->with('success', 'Post created successfully');
     }
 
     /**
