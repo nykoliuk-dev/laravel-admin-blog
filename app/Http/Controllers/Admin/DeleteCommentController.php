6b9eb215f8a5960db2bc8a04fc86142d4bd10ead
Roman <webdeveloverroman@gmail.com>
2026-02-16 18:47:11 +0200

    feat(comments): implement delete action in DeleteCommentController

--- app/Http/Controllers/Admin/DeleteCommentController.php
+++ app/Http/Controllers/Admin/DeleteCommentController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class DeleteCommentController extends Controller
@@ -10,8 +12,12 @@ class DeleteCommentController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, Comment $comment): RedirectResponse
     {
-        //
+        $comment->delete();
+
+        return redirect()
+            ->route('admin.comments.index')
+            ->with('success', 'Comment deleted successfully');
     }
 }
