113d35a042a3b9df61fa83ce0998f25896a2114a
Roman <webdeveloverroman@gmail.com>
2026-02-16 18:34:45 +0200

    feat(comments): implement Comment List Page with pagination and DTO

--- app/Http/Controllers/Admin/ListCommentsController.php
+++ app/Http/Controllers/Admin/ListCommentsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Domain\Comments\Queries\ListCommentsQuery;
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class ListCommentsController extends Controller
@@ -10,8 +12,16 @@ class ListCommentsController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, ListCommentsQuery $query)
     {
-        //
+        $commentPaginator = $query->handle(
+            page: (int) $request->query('page', 1),
+            perPage: (int) $request->query('perPage', 3),
+        );
+
+        return view('admin.comments.index', [
+            'title' => 'Comment List Page',
+            'comments' => $commentPaginator,
+        ]);
     }
 }
