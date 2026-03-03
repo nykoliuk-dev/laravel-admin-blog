2339cc2c4b8272e201ab30736574e783d90962dc
Roman <webdeveloverroman@gmail.com>
2026-03-03 11:25:34 +0200

    refactor: unify docblock style

--- app/Console/Commands/ImportOldBlogData.php
+++ app/Console/Commands/ImportOldBlogData.php
@@ -14,15 +14,11 @@ use Illuminate\Support\Facades\DB;
 class ImportOldBlogData extends Command
 {
     /**
-     * The name and signature of the console command.
-     *
      * @var string
      */
     protected $signature = 'blog:import-old';
 
     /**
-     * The console command description.
-     *
      * @var string
      */
     protected $description = 'Import data from old blog database';
--- app/Models/User.php
+++ app/Models/User.php
@@ -15,8 +15,6 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
      * @var list<string>
      */
     protected $fillable = [
@@ -26,8 +24,6 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
-     *
      * @var list<string>
      */
     protected $hidden = [
@@ -36,8 +32,6 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
-     *
      * @return array<string, string>
      */
     protected function casts(): array
